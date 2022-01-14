154b83d3816be84f78c99c4617bc15c982017de1
David Gaspar <davidgaspar.dev@gmail.com>
2022-01-14 15:51:50 -0300

    Rewrite path to api (/projeto/*)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: "/projeto/:path*",
+      destination: "/api/:path*"
+    }
+  ]
 }
 
 module.exports = nextConfig
