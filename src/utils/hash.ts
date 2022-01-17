5a6e79c38cea3516924f762b33023bfedfc48715
David Gaspar <davidgaspar.dev@gmail.com>
2022-01-17 09:53:10 -0300

    isTextMd5 function created - close #11

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -3,3 +3,15 @@ import crypto from "crypto";
 export function textToMd5(text: string) {
     return crypto.createHash('md5').update(text).digest('hex');
 }
+
+const hex = "0123456789abcdef";
+
+export function isTextMd5(text: string)  {
+    if(text.length !== 32) return false;
+
+    for(const letter of text) {
+        if(!hex.includes(letter)) return false;
+    }
+
+    return true;
+}
