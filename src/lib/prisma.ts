de42c2aee0327bfed710e48173931dfb4523cf02
David Gaspar <davidgaspar.dev@gmail.com>
2022-01-17 14:13:13 -0300

    Fixed problem with Prisma in development environment - fixed #13

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 
+// For development
+declare global {
+    var prisma: PrismaClient | undefined
+}
+
 export default abstract class Prisma {
     private static client?: PrismaClient;
 
     protected getConnect(): PrismaClient {
-        if(Prisma.client == null) {
-            Prisma.client = new PrismaClient();
+        if(process.env.NODE_ENV === 'production') {
+            if(Prisma.client == null) {
+                Prisma.client = new PrismaClient();
+            }
+            return Prisma.client;
+        } else {
+            if(global.prisma == null) {
+                global.prisma = new PrismaClient();
+            }
+            return global.prisma;
         }
-        return Prisma.client;
     }
 }
