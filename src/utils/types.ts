b696eaa147934a09f47773bd59f3296c881beaa3
David Gaspar <davidgaspar.dev@gmail.com>
2022-01-18 09:42:06 -0300

    ComponentData type created - close #20

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,16 @@ export type UserData = {
     username: string;
     password: string;
 }
+
+export type ComponentData = {
+    id?: number;
+    name: string;
+    gtin: string;
+    systemType: "ONGRID" | "OFFGRID";
+    componentGroup: "PERFIL" | "MODULE" | "INVERSOR" | "CABOS" | "CONECTORES" | "BATERIAS";
+    height: number;
+    width: number;
+    depth: number;
+    weight: number;
+    netWeight: number;
+}
