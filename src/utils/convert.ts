cfd029712a9ae535b083703d31cecedbba31d78a
David Gaspar <davidgaspar.dev@gmail.com>
2022-01-18 13:27:15 -0300

    mapToQuery function created - close #24

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -8,3 +8,23 @@ export function base64ToString(base64: string) {
     const buffer = Buffer.from(base64, "base64");
     return buffer.toString("utf-8");
 }
+
+export function mapToQuery(map: { [key: string]: any }) {
+    let result = '';
+
+    Object.keys(map).forEach(key => {
+        if(map[key] == null) {
+            delete map[key];
+        }
+    });
+
+    const keys = Object.keys(map);
+    for(let i = 0; i < keys.length; i++) {
+        result += `${keys[i]}=${map[keys[i]]}`;
+        if(i !== (keys.length - 1)) {
+            result += "&";
+        }
+    }
+
+    return result;
+}
