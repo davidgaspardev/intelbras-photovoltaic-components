1e71db70769697542e06607996c4ce857ba7f3df
David Gaspar <davidgaspar.dev@gmail.com>
2022-01-18 10:18:33 -0300

    Component item created - close #19

---
+++ src/components/ComponentItem.tsx
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+import { ComponentData } from "../utils/types";
+import { Text } from "../components/base/Text";
+import { Flex } from "./base/Flex";
+import { Box } from "./base/Box";
+
+type Props = {
+    data: ComponentData;
+}
+
+export default function ComponentItem(props: Props) {
+    const { data } = props;
+
+    return (
+        <ComponentItemStyled>
+            <h3>{data.name}</h3>
+            <h4>{data.gtin}</h4>
+
+            <Flex className="component-categories">
+                <Box width="50%">
+                    <Text className="value-label" fontSize="9pt">Tipo do sistema</Text>
+                    <Text fontWeight="bold">{data.systemType}</Text>
+                </Box>
+                <Box width="50%">
+                    <Text className="value-label" fontSize="9pt">Grupo do componente</Text>
+                    <Text fontWeight="bold">{data.componentGroup}</Text>
+                </Box>
+            </Flex>
+
+            <Flex>
+                <Box width="33%">
+                    <Text className="value-label" fontSize="9pt">Largura</Text>
+                    <Text><strong>{data.width.toString()}</strong> cm</Text>
+                </Box>
+                <Box width="33%">
+                    <Text className="value-label" fontSize="9pt">Altura</Text>
+                    <Text><strong>{data.height.toString()}</strong> cm</Text>
+                </Box>
+                <Box width="33%">
+                    <Text className="value-label" fontSize="9pt">Profund.</Text>
+                    <Text><strong>{data.depth.toString()}</strong> cm</Text>
+                </Box>
+            </Flex>
+
+            <hr/>
+
+            <Flex>
+                <Box width="50%">
+                    <Text className="value-label" fontSize="9pt">Peso bruto</Text>
+                    <Text><strong>{data.weight.toString()}</strong> Kg</Text>
+                </Box>
+                <Box width="50%">
+                    <Text className="value-label" fontSize="9pt">Peso líquido</Text>
+                    <Text><strong>{data.netWeight.toString()}</strong> Kg</Text>
+                </Box>
+            </Flex>
+
+        </ComponentItemStyled>
+    );
+}
+
+const ComponentItemStyled = styled.div`
+    max-width: 380px;
+    width: 100%;
+    margin: 8px;
+    padding: 16px;
+    background-color: #41BF79;
+    color: white;
+    border-radius: 5px;
+
+    div.component-categories {
+        background-color: #4CA473;
+        padding: 12px 0;
+        margin: 8px 0;
+        border-radius: 5px;
+    }
+
+    p.value-label {
+        margin-bottom: 4px;
+    }
+
+    p, h3, h4 {
+        padding: 0px 10px;
+    }
+
+    h4 {
+        font-weight: normal;
+        opacity: .75;
+        margin-top: 3px;
+        font-size: 10pt;
+    }
+
+    hr {
+        border: 1px solid #4CA473;
+        margin: 10px 0;
+    }
+`;
