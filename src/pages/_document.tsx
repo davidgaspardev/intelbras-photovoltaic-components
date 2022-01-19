d5d29d2a83e2c3b5eb9d31bb9b3ba9e7a1fc0948
David Gaspar <davidgaspar.dev@gmail.com>
2022-01-19 08:02:05 -0300

    Open Graph Protocol added - close #33

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,8 +36,18 @@ export default class MyDocument extends Document {
                     <meta charSet="utf-8"/>
                     <meta name="theme-color" content="#4FAE72"/>
                     <meta
-                        name="description"
-                        content="Intelbras Project"/>
+                      name="description"
+                      content="Intelbras Project"/>
+
+                    <meta
+                      property="og:title"
+                      content="Intelbras Photovoltaic Components"/>
+                    <meta
+                      property="og:description"
+                      content="Photovoltaic components inventory manager. Photovoltaic components are part of the resources present in Intelbras' arsenal, which are used for product development."/>
+                    <meta 
+                      property="og:image"
+                      content="https://firebasestorage.googleapis.com/v0/b/myself-dg.appspot.com/o/interview%2Fintelbras%2Fcover.png?alt=media&token=7b97bec8-d9b6-41ad-950c-243ac4cf8622" />
                 </Head>
                 <body>
                     <Main />
